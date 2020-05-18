ec69ebeb361bd6530ae971db96e38ae8df888506
Toy Pongsatorn <>
2020-05-18 16:52:45 +0700

    change backend logo

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<a href="{{ route('backend.dashboard') }}" class="brand-link">
-		<img src="{{ asset('images/backend/avatar.jpg') }}"
+		<img src="{{ asset('images/backend/logo.png') }}"
 		alt="AdminLTE Logo"
 		class="brand-image img-circle elevation-3"
 		style="opacity: .8">
